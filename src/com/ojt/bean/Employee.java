591e3b53e13a132c516c04e6dfc75ddc525e7cc5
www.onlinejavatrainer.com <info@onlinejavatrainer.com>
2014-10-09 02:31:18 -0400

    create getter-setter and parameterized constructor for Employee class

--- src/com/ojt/bean/Employee.java
+++ src/com/ojt/bean/Employee.java
@@ -5,5 +5,34 @@ public class Employee {
 	private long id;
 	private String fname;
 	private String lname;
+	
+	public Employee(){}
+	
+	public Employee(long id, String fname, String lname) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
 
 }
